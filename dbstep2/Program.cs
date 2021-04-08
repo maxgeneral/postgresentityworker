56fe48a57c0b6ec72712a61e79b32108cc62d4ec
maxgeneral <80332856+maxgeneral@users.noreply.github.com>
2021-04-09 02:26:37 +0300

    Add project files.

---
+++ dbstep2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dbstep2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
