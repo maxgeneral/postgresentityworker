7188360ef4f4ae439df7c67d508ccb7b3c6ca234
maxgeneral <80332856+maxgeneral@users.noreply.github.com>
2021-04-09 03:00:22 +0300

    Добавляем все стандартные экраны, чтобы можно было их редактировать

---
+++ webclient/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using webclient.Data;
+
+[assembly: HostingStartup(typeof(webclient.Areas.Identity.IdentityHostingStartup))]
+namespace webclient.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
